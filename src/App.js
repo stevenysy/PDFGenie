2c00ea07bbb88b93641bc841405367425a92e75a
Steven Yi <stevenjxhc@gmail.com>
2024-06-24 06:24:20 +0800

    add components to main ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ChatComponent from "./components/ChatComponent";
+import { useState } from "react";
+import { Layout, Typography } from "antd";
+import PdfUploader from "./components/PdfUploader";
+import Conversation from "./components/Conversation";
+
+const chatComponentStyle = {
+  position: "fixed",
+  bottom: "0",
+  width: "80%",
+  left: "10%",
+  marginBottom: "20px",
+};
+
+const pdfUploaderStyle = {
+  margin: "auto",
+  paddingTop: "80px",
+  paddingLeft: "20%",
+};
+
+const conversationStyle = {
+  height: "50%",
+  overflowY: "auto",
+};
 
 function App() {
+  const [conversation, setConversation] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { Header, Content } = Layout;
+  const { Title } = Typography;
+
+  const handleResp = (question, answer) => {
+    setConversation([...conversation, { question, answer }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout style={{ height: "100vh", backgroundColor: "white" }}>
+        <Header style={{ display: "flex", alignItems: "center" }}>
+          <Title style={{ color: "white" }}>Next AI</Title>
+        </Header>
+        <Content style={{ width: "80%" }}>
+          <div style={pdfUploaderStyle}>
+            <PdfUploader />
+          </div>
+
+          <br />
+          <br />
+          <div style={conversationStyle}>
+            <Conversation content={conversation} isLoading={isLoading} />
+          </div>
+          <br />
+          <br />
+        </Content>
+        <div style={chatComponentStyle}>
+          <ChatComponent
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
+            handleResp={handleResp}
+          />
+        </div>
+      </Layout>
+    </>
   );
 }
 
