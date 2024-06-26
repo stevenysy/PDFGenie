31583d87ef7145d0bf2473f73d1877c4ede17ae0
Steven Yi <stevenjxhc@gmail.com>
2024-06-26 12:00:17 +0800

    implement chat mode

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,6 +1,11 @@
-import { Input } from "antd";
-import { useState } from "react";
+import { Button, Input } from "antd";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import Speech from "speak-tts";
+import { AudioOutlined } from "@ant-design/icons";
 
 const { Search } = Input;
 
@@ -13,6 +18,82 @@ const searchContainerStyle = {
 
 const ChatComponent = ({ isLoading, setIsLoading, handleResp }) => {
   const [searchValue, setSearchValue] = useState("");
+  const [isChatModeOn, setIsChatModeOn] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [speech, setSpeech] = useState();
+
+  // Speech recognition
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
+
+  useEffect(() => {
+    const speech = new Speech();
+    speech
+      .init({
+        volume: 1,
+        lang: "en-US",
+        rate: 1,
+        pitch: 1,
+        voice: "Google US English",
+        splitSentences: true,
+      })
+      .then((data) => {
+        console.log(`speech is ready, voices are available: `, data);
+        setSpeech(speech);
+      })
+      .catch((err) => {
+        console.error(`An error occurred while initializing: ${err}`);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!listening && !!transcript) {
+      (async () => await onSearch(transcript))();
+      setIsRecording(false);
+    }
+  }, [listening, transcript]);
+
+  const talk = (content) => {
+    speech
+      .speak({
+        text: content,
+        queue: false,
+        listeners: {
+          onstart: () => {
+            console.log("start utterance");
+          },
+          onend: () => {
+            console.log("end utterance");
+          },
+          onresume: () => {
+            console.log("resume utterance");
+          },
+          onboundary: (event) => {
+            console.log(
+              `${event.name} boundary reached after ${event.elapsedTime} milliseconds`,
+            );
+          },
+        },
+      })
+      .then(() => {
+        console.log("Success!");
+        userStartConvo();
+      })
+      .catch((err) => {
+        console.log("An error occurred: ", err);
+      });
+  };
+
+  const userStartConvo = () => {
+    SpeechRecognition.startListening();
+    setIsRecording(true);
+    resetTranscript();
+  };
 
   const onSearch = async (question) => {
     setSearchValue("");
@@ -25,6 +106,9 @@ const ChatComponent = ({ isLoading, setIsLoading, handleResp }) => {
         },
       });
       handleResp(question, response.data);
+      if (isChatModeOn) {
+        talk(response.data);
+      }
     } catch (error) {
       console.error(`Error: ${error}`);
       handleResp(question, error);
@@ -37,17 +121,56 @@ const ChatComponent = ({ isLoading, setIsLoading, handleResp }) => {
     setSearchValue(e.target.value);
   };
 
+  const handleChatModeClick = () => {
+    setIsChatModeOn(!isChatModeOn);
+    setIsRecording(false);
+    SpeechRecognition.stopListening();
+  };
+
+  const handleRecordingClick = () => {
+    if (isRecording) {
+      setIsRecording(false);
+      SpeechRecognition.stopListening();
+    } else {
+      setIsRecording(true);
+      SpeechRecognition.startListening();
+    }
+  };
+
   return (
     <div style={searchContainerStyle}>
-      <Search
-        placeholder="Ask me anything about the uploaded documents!"
-        enterButton="Ask"
+      {!isChatModeOn && (
+        <Search
+          placeholder="Ask me anything about the uploaded documents!"
+          enterButton="Ask"
+          size="large"
+          onSearch={onSearch}
+          loading={isLoading}
+          value={searchValue}
+          onChange={handleChange}
+        />
+      )}
+      <Button
+        type="primary"
         size="large"
-        onSearch={onSearch}
-        loading={isLoading}
-        value={searchValue}
-        onChange={handleChange}
-      />
+        danger={isChatModeOn}
+        onClick={handleChatModeClick}
+        style={{ marginLeft: "5px" }}
+      >
+        Chat mode: {isChatModeOn ? "On" : "Off"}
+      </Button>
+      {isChatModeOn && (
+        <Button
+          type="primary"
+          icon={<AudioOutlined />}
+          size="large"
+          danger={isRecording}
+          onClick={handleRecordingClick}
+          style={{ marginLeft: "5px" }}
+        >
+          {isRecording ? "Recording..." : "Click to record"}
+        </Button>
+      )}
     </div>
   );
 };
